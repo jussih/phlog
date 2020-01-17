08d9c05576a6eb0af09d60ba5d7f8b0ba59f0497
Jussi Heikkilä <jussih@gmail.com>
2020-01-17 19:30:36 +0200

    Keyboard up/down changes active doc

--- lib/phlog_web/live/documents_live.ex
+++ lib/phlog_web/live/documents_live.ex
@@ -7,7 +7,7 @@ defmodule PhlogWeb.DocumentsLive do
 
   def render(assigns) do
     ~L"""
-    <div>
+    <div phx-keydown="keydown" phx-target="window">
       <%= for document <- @documents do %>
         <div class="<%= if document.id == @active_document do %>active<% end %>" phx-click="document_click<%= document.id %>">
           <b><%= document.title %></b>: <%= document.html %>
@@ -21,13 +21,30 @@ defmodule PhlogWeb.DocumentsLive do
     documents = Repository.list_repository_documents()
     {:ok, socket
           |> assign(:documents, documents)
-          |> assign(:active_document, 0)
+          |> assign(:active_document, hd(documents).id)
     }
   end
 
   def handle_event("document_click" <> document_id, _payload, socket) do
     Logger.debug(fn -> "Document clicked: #{document_id}" end)
     {:noreply, assign(socket, :active_document, String.to_integer(document_id))}
+  end
+
+  def handle_event("keydown", key, socket) do
+    Logger.debug(fn -> "Key pressed #{inspect key}" end)
+    {:noreply, assign(socket, :active_document, get_active_document(key, socket))}
+  end
+
+  def get_active_document(
+    key,
+    %Phoenix.LiveView.Socket{assigns: %{documents: documents, active_document: active_document}} = socket
+  ) do
+    active_idx = Enum.find_index(documents, fn d -> d.id == active_document end)
+    case key["key"] do
+      "ArrowUp" -> Enum.at(documents, rem(active_idx - 1, Enum.count(documents))).id
+      "ArrowDown" -> Enum.at(documents, rem(active_idx + 1, Enum.count(documents))).id
+      _ -> active_document
+    end
   end
 
   def handle_event(event, payload, socket) do
